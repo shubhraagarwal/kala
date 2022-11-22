22013f066a7278c27853236162aa57dc3c438d1d
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-11-22 14:05:54 +0530

    Aplha teaser

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Draw your minds out</h1>
+    <h3>Alpha Coming Soon!</h3>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
